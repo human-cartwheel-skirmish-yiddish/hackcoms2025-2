38184a70a8b9cb872eb89d21a61602c40afebc79
anna-leon88 <241459056+anna-leon88@users.noreply.github.com>
2025-11-01 16:17:05 -0400

    questions and answers

--- src/test.js
+++ src/test.js
@@ -1,3 +1,13 @@
-const yes = "yes";
+/*const yes = "yes";
+export { yes };*/
 
-export { yes };
+
+randint = Math.floor(Math.random()*);
+console.log(laws[0]["q"]);
+
+/*
+question bank
+clickable buttons
+answers highlighted
+switches to diferent random question when prompted
+*/
