940c03bc51d54bca26f04d3ed826019defa0dc7e
anna-leon88 <241459056+anna-leon88@users.noreply.github.com>
2025-11-01 21:10:55 -0400

    question, answer, and points

--- src/cqselection.js
+++ src/cqselection.js
@@ -1,12 +1,37 @@
 //random selection: categories and questions
 import {years,names,laws} from './flashcards.js';
-let categorylist = [years,names,laws];
-let category = categorylist[Math.floor(Math.random()*3)];
+function questionMaking(){
+    let categorylist = [years,names,laws];
+    let category = categorylist[Math.floor(Math.random()*3)];
 //random q selection
-let randomQuestion = Math.floor(Math.random()*5);
-let chosenQuestion = category[randomQuestion]["q"];
-//console.log(chosenQuestion);
+    let randomQuestion = Math.floor(Math.random()*4);
+    let chosenQuestion = category[randomQuestion]["q"];
+    let chosenAnswer = category[randomQuestion]["a"];
+    console.log(chosenQuestion);
 
+    let array = [];
+    for (let i = 0; i < 4; i++){
+        let answerarray = [category[i]["a"]];
+        array = array.concat(answerarray);
+    }
 
-let chosenAnswer = category[randomQuestion]["a"]
-console.log(chosenAnswer);
+    console.log(array);
+    return [array, chosenAnswer];
+}
+
+let points = 0;
+for (let i = 0; i < 12; i++){
+    let [array, chosenAnswer] = questionMaking();
+    let user_answer = prompt("What is the answer? (answer in array# 0, 1, 2, or 3)");
+    if (user_answer == array.indexOf(chosenAnswer)){
+        alert("You got it right!");
+        points += 1;
+    }
+    else{
+        alert("You got it wrong! The correct answer is " + chosenAnswer);
+    }
+    console.log(points);
+}
+if (points == 12){
+    console.log("Congrats, you got the highest possible score!");
+}
