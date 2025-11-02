5e8cb73fca184a016b62c4b9fb3ed8f8e6e09e57
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2025-11-01 23:31:35 -0400

    Add colors depending on answers

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import "./styles/mcq.css";
 import { questionMaking } from "./cqselection";
 
 let points = 0;
+const QUESTIONS_MAX = 4;
 const questionElt = document.getElementById("text--question");
 const answersElt = document
   .getElementById("container--answers")
   .querySelectorAll("li");
 const pointsElt = document.getElementById("text--points");
+const continueBtnElt = document.getElementById("button--continue");
+const feedbackElt = document.getElementById("text--feedback");
 
 console.log(answersElt, typeof answersElt);
 
@@ -27,40 +30,65 @@ for (let index = 0; index < 4; index++) {
   answersElt[index].innerHTML = answers[index];
 }
 
+continueBtnElt.addEventListener("click", () => {
+  questionsDone++;
+  if (questionsDone < QUESTIONS_MAX) {
+    const newQuestionSet = questionMaking();
+    answers = newQuestionSet["answers"];
+    answer = newQuestionSet["answer"];
+    question = newQuestionSet["question"];
+
+    console.log(answers, question, answer);
+    questionElt.innerHTML = question;
+
+    // Populate answers
+    for (let index = 0; index < 4; index++) {
+      answersElt[index].innerHTML = answers[index];
+    }
+    continueBtnElt.style.display = "none";
+    feedbackElt.innerHTML = "";
+  } else {
+    feedbackElt.innerHTML = `Congrats! You're done with the quiz! Your score was ${points}/${QUESTIONS_MAX}`;
+  }
+  answersElt.forEach(elt => {
+    elt.style.backgroundColor = "";
+    elt.style.color = "";
+    elt.style.borderColor = "";
+  })
+});
+
 answersElt.forEach(function (elt) {
   elt.addEventListener("click", () => {
-    // console.log(pointsElt.innerHTML);
-    if (questionsDone < 4) {
+    let feedback = "";
+    if (questionsDone < QUESTIONS_MAX) {
       user_answer = elt.innerHTML;
 
-      // let user_answer = prompt("What is the answer? (answer in array# 0, 1, 2, or 3)");
       if (user_answer == answers[answers.indexOf(answer)]) {
-        console.log("You got it right!");
+        feedback = "You got it right!";
+        elt.style.backgroundColor = "green";
+        elt.style.color = "white";
         points += 1;
       } else {
-        console.log("You got it wrong! The correct answer is " + answer);
+        feedback = "You got it wrong! The correct answer is " + answer;
+        answersElt.forEach((elt2) => {
+          if (elt2.innerHTML != answer) {
+            elt2.style.backgroundColor = "red";
+          }
+          else {
+            elt2.style.backgroundColor = "green";
+          }
+          elt2.style.color = "white";
+
+          elt2.style.borderColor = "black";
+        });
       }
+
       pointsElt.innerHTML = points;
+      feedbackElt.innerHTML = feedback;
+      continueBtnElt.style.display = "inline-block";
 
-
-      if (points == 12) {
+      if (points == QUESTIONS_MAX) {
         console.log("Congrats, you got the highest possible score!");
-      }
-
-      questionsDone++;
-      if (questionsDone < 12) {
-        const newQuestionSet = questionMaking();
-        answers = newQuestionSet["answers"];
-        answer = newQuestionSet["answer"];
-        question = newQuestionSet["question"];
-
-        console.log(answers, question, answer);
-        questionElt.innerHTML = question;
-
-        // Populate answers
-        for (let index = 0; index < 4; index++) {
-          answersElt[index].innerHTML = answers[index];
-        }
       }
     }
   });
